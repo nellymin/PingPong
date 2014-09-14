8968b48c24060a416d458e13cd98d138a3764e8f
java-team-date <nikodimov_86@yahoo.de>
2014-09-14 09:58:16 +0200

    Update PingPongPanel.java
    Adding result pane & fixing the first player movement

--- PingPong/PingPong/src/PingPongPanel.java
+++ PingPong/PingPong/src/PingPongPanel.java
@@ -1,60 +1,77 @@
 import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class PingPongPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static final int BALL_SIZE = 25;
 	private static final int STICK_SIZE = 100;
-	private int ballStartX = 100;  
-	private int ballStartY = 100; 
+	private int ballStartX = 380; // Lyudmil
+	private int ballStartY = 250; // Lyudmil
 	private int ballStepX = 0;
 	private int ballStepY = 0;
-	private int stickStart = 100;
-	private int stickTwoStart = 100;
+	private int stickOneStart = 200; // Lyudmil
+	private int stickTwoStart = 200; // Lyudmil
 	private int stickStep = 0;
-	
+	private int firstPlayerResult = 0; // Lyudmil
+	private int secondPlayerResult = 0; // Lyudmil
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.fillOval(ballStartX, ballStartY, BALL_SIZE, BALL_SIZE);
-		g.fillRect(1, stickStart, 5, STICK_SIZE);
-		g.fillRect((int)getSize().getWidth()-6, stickTwoStart, 5, STICK_SIZE);
+		g.fillRect(1, stickOneStart, 5, STICK_SIZE);
+		g.fillRect((int) getSize().getWidth() - 6, stickTwoStart, 5, STICK_SIZE);
+		g.drawString("Player 1: " + firstPlayerResult, 5, 10); // Lyudmil
+		g.drawString("Player 2: " + secondPlayerResult, getWidth() - 60, 10); // Lyudmil
 	}
-			
+
 	public void moveBall() {
 		ballStartX += this.ballStepX;
 		ballStartY += this.ballStepY;
-		if(ballStartX + BALL_SIZE > getSize().getWidth()) {
+		if (ballStartX + BALL_SIZE > getSize().getWidth()) {
 			ballStepX = -1;
 		}
-		
-		if(ballStartX < 0) {
+
+		if (ballStartX < 0) {
 			ballStepX = 1;
 		}
-		
-		if(ballStartY + BALL_SIZE > getSize().getHeight()) {
+
+		if (ballStartY + BALL_SIZE > getSize().getHeight()) {
 			ballStepY = -1;
 		}
-		
-		if(ballStartY < 0) {
+
+		if (ballStartY < 0) {
 			ballStepY = 1;
 		}
 	}
-	
+
 	public void moveStick() {
 
-		this.stickStart += this.stickStep;
-		
+		this.stickOneStart += this.stickStep;
+
 	}
-	
+
 	public void setStepX(int stepX) {
 		this.ballStepX = stepX;
 	}
-	
+
 	public void setStepY(int stepY) {
 		this.ballStepY = stepY;
 	}
-	
-	public void setStickMovement(int step){
+
+	public void setStickMovement(int step) {
 		this.stickStep = step;
+		if (stickOneStart > getSize().getHeight()) { // Lyudmil
+			stickOneStart = (int) getSize().getHeight() - 100; // Lyudmil
+			stickStep = 0; // Lyudmil
+		}
+		if (stickOneStart < 0) { // Lyudmil
+			stickOneStart = 0; // Lyudmil
+			stickStep = 0; // Lyudmil
+		}
 	}
 }
