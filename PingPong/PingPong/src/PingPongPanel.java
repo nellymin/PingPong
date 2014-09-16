f777be808641bfc399c7aeca8aa6e041f3f0aff7
SCiENiDE <dimitrovite@abv.bg>
2014-09-16 14:18:08 +0300

    Ball angles
    The ball will bounce off the sticks in a number of angles, according to
    the section of the stick it hit.

--- PingPong/PingPong/src/PingPongPanel.java
+++ PingPong/PingPong/src/PingPongPanel.java
@@ -13,14 +13,14 @@ public class PingPongPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private static final int BALL_SIZE = 25;
 	private static final int STICK_SIZE = 100;
-	private int ballStartX = 380; // Lyudmil
-	private int ballStartY = 250; // Lyudmil
+	private int ballX = 380; // Lyudmil
+	private int ballY = 250; // Lyudmil
 	private int ballStepX = 0;
 	private int ballStepY = 0;
-	private int stickOneStart = 200; // Lyudmil
-	private int stickTwoStart = 200; // Lyudmil
-	private int stickStep = 0;
-	private int stickStepTwo = 0;
+	private int stickOneY = 200; // Lyudmil
+	private int stickTwoY = 200; // Lyudmil
+	private int stickOneStep = 0;
+	private int stickTwoStep = 0;
 	private int firstPlayerResult = 0; // Lyudmil
 	private int secondPlayerResult = 0; // Lyudmil
 	private boolean isPaused = false; // Dani
@@ -28,9 +28,9 @@ public class PingPongPanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.fillOval(ballStartX, ballStartY, BALL_SIZE, BALL_SIZE);
-		g.fillRect(1, stickOneStart, 5, STICK_SIZE);
-		g.fillRect((int) getSize().getWidth() - 6, stickTwoStart, 5, STICK_SIZE);
+		g.fillOval(ballX, ballY, BALL_SIZE, BALL_SIZE);
+		g.fillRect(1, stickOneY, 5, STICK_SIZE);
+		g.fillRect((int) getSize().getWidth() - 6, stickTwoY, 5, STICK_SIZE);
 		g.drawString("Player 1: " + firstPlayerResult, 5, 10); // Lyudmil
 		g.drawString("Player 2: " + secondPlayerResult, getWidth() - 60, 10); // Lyudmil
 		
@@ -43,58 +43,60 @@ public class PingPongPanel extends JPanel {
 	
 	public void moveBall() {
 		if(!isPaused){
-		ballStartX += this.ballStepX;
-		ballStartY += this.ballStepY;
+			ballX += this.ballStepX;
+			ballY += this.ballStepY;
+			
+			if (ballX + BALL_SIZE > getSize().getWidth()) {
+				if (ballY < stickTwoY
+						|| ballY > stickTwoY + STICK_SIZE) {
+					SoundEffect.SCORE.play();
+					firstPlayerResult++;
+					win(1);
+				}
+				else {
+					SoundEffect.PAD_BOUNCE.play();
+					setBallStepXY();
+				}
+				// мини задача: за по- добър визуален ефект топчето да излиза извън
+				// очертанието на полето.
+			}
+	
+			if (ballX < 0) {
+				if (ballY < stickOneY
+						|| ballY > stickOneY + STICK_SIZE) {
+					SoundEffect.SCORE.play();
+					secondPlayerResult++;
+					win(2);
+				}
+				else {
+					SoundEffect.PAD_BOUNCE.play();
+					setBallStepXY();
+				}
+			}
 		
-		if (ballStartX + BALL_SIZE > getSize().getWidth()) {
-			ballStepX = -1;
-			if (ballStartY < stickTwoStart
-					|| ballStartY > stickTwoStart + STICK_SIZE) {
-				SoundEffect.SCORE.play();
-				firstPlayerResult++;
-				win(1);
+			if (ballY + BALL_SIZE > getSize().getHeight()) {
+				ballStepY *= -1;
+				SoundEffect.WALL_BOUNCE.play();
 			}
-			else
-				SoundEffect.PAD_BOUNCE.play();
-			// мини задача: за по- добър визуален ефект топчето да излиза извън
-			// очертанието на полето.
-		}
-
-		if (ballStartX < 0) {
-			ballStepX = 1;
-			if (ballStartY < stickOneStart
-					|| ballStartY > stickOneStart + STICK_SIZE) {
-				SoundEffect.SCORE.play();
-				secondPlayerResult++;
-				win(2);
+		
+			if (ballY < 0) {
+				ballStepY *= -1;
+				SoundEffect.WALL_BOUNCE.play();
 			}
-			else
-				SoundEffect.PAD_BOUNCE.play();
-		}
-
-		if (ballStartY + BALL_SIZE > getSize().getHeight()) {
-			ballStepY = -1;
-			SoundEffect.WALL_BOUNCE.play();
-		}
-
-		if (ballStartY < 0) {
-			ballStepY = 1;
-			SoundEffect.WALL_BOUNCE.play();
-		}
 		}
 	}
 
 	public void moveStick() {
-		this.stickOneStart += this.stickStep;
-		if (stickOneStart <= 1
-				|| stickOneStart >= getSize().getHeight() - STICK_SIZE) { // Niki
+		this.stickOneY += this.stickOneStep;
+		if (stickOneY <= 1
+				|| stickOneY >= getSize().getHeight() - STICK_SIZE) { // Niki
 			// 14
-			this.stickStep = 0; // Niki 14
+			this.stickOneStep = 0; // Niki 14
 		}
-		this.stickTwoStart += this.stickStepTwo;
-		if (stickTwoStart <= 1
-				|| stickTwoStart >= getSize().getHeight() - STICK_SIZE) {
-			this.stickStepTwo = 0;
+		this.stickTwoY += this.stickTwoStep;
+		if (stickTwoY <= 1
+				|| stickTwoY >= getSize().getHeight() - STICK_SIZE) {
+			this.stickTwoStep = 0;
 		}
 	}
 
@@ -107,11 +109,11 @@ public class PingPongPanel extends JPanel {
 	}
 
 	public void setStickStep(int step) {
-		this.stickStep = step;
+		this.stickOneStep = step;
 	}
 
 	public void setStickStepTwo(int step) {
-		this.stickStepTwo = step;
+		this.stickTwoStep = step;
 	}
 	
 	public boolean getPaused() {
@@ -140,14 +142,46 @@ public class PingPongPanel extends JPanel {
 		JOptionPane.showMessageDialog(null, message, "PiNg PoNg reSult",
 				JOptionPane.WARNING_MESSAGE);
 	}
+	
+	private void setBallStepXY() {
+		int steps = 5;
+		int[] stickSectionCoord = new int[steps];
+		int[] ySteps = new int[steps];
+		for (int i = 0; i < steps; i++) {
+			ySteps[i] = i <= steps / 2 ? (steps / 2 - i) * -1 : i - steps / 2;
+		}
+		int sectionSize = STICK_SIZE / stickSectionCoord.length;
+		
+		if (ballStepX == 1) {
+			//stickTwoY
+			for (int i = 0; i < steps; i++) {
+				//initialize array with the coordinates of stick sectors
+				stickSectionCoord[i] = i * sectionSize + stickTwoY;
+			}
+		}
+		else {
+			//initialize array with the coordinates of stick sectors
+			for (int i = 0; i < steps; i++) {
+				stickSectionCoord[i] = i * sectionSize + stickOneY;
+			}
+		}
+		for (int i = 0; i < steps; i++) {
+			if (ballY > stickSectionCoord[i] &&
+					ballY < stickSectionCoord[i + 1]) {
+				ballStepY = ySteps[i];
+				ballStepX *= -1;
+				break;
+			}
+		}
+	}
 
 	void centeringStickAndBall() { // Dani
-		ballStartX = 380;
-		ballStartY = 250;
+		ballX = 380;
+		ballY = 250;
 		randomBallMovement();
 		setStickStep(0);
 		setStickStepTwo(0);
-		this.stickOneStart = getHeight() / 2 - 80;  // setting the sticks to start from the center
-		this.stickTwoStart = getHeight() / 2 - 80;  
+		this.stickOneY = getHeight() / 2 - 80;  // setting the sticks to start from the center
+		this.stickTwoY = getHeight() / 2 - 80;  
 	}
 }
